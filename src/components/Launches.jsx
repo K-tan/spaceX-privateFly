e21f62fe03e32cb7132fc84af6b38bf0e6530769
Katan Patel <katanpatel86@gmail.com>
2019-08-20 09:35:09 +0100

    retrieves data from spacex api which displays 10 most recent launches

--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -6,22 +6,13 @@ class Launches extends Component {
     launches: []
   };
 
-  componentDidMount() {
-    axios.get(`https://api.spacexdata.com/v3/launches`).then(res => {
-      console.log(res);
-      this.setState({ launches: res.data });
-    });
-  }
-
   render() {
     return (
       <ul>
         {this.state.launches.map(launch => (
           <li key={launch.flight_number}>
             <h6>Flight Number: {launch.flight_number}</h6>
-            <h6>
-              Launch Date: {new Date(launch.launch_date_unix).toUTCString()}
-            </h6>
+            <h6>Launch Date: {launch.launch_date_utc}</h6>
             <h6>Rocket Name: {launch.rocket.rocket_name}</h6>
             <h6>Launch Year: {launch.launch_year}</h6>
             <h6>Launch Success: {launch.launch_success}</h6>
@@ -31,6 +22,18 @@ class Launches extends Component {
       </ul>
     );
   }
+
+  componentDidMount = () => {
+    axios
+      .get(
+        // `https://api.spacexdata.com/v3/launches/?sort=launch_date_utc&order=asc`
+        `https://api.spacexdata.com/v3/launches/past?sort=launch_date_utc&order=desc&limit=10`
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ launches: res.data });
+      });
+  };
 }
 
 export default Launches;
